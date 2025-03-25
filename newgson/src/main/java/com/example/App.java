2dc033f1a7b00319b7faeefc2ef5b8a08fd81faa
lidan <lilidan2001@hotmail.com>
2025-03-26 00:03:10 +0900

    20250325 修正しました

--- newgson/src/main/java/com/example/App.java
+++ newgson/src/main/java/com/example/App.java
@@ -1,10 +1,20 @@
 package com.example;
 
+import com.google.gson.Gson;
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Gson gson = new Gson();
+
+        Person person = new Person("Alice", 25);
+        String json = gson.toJson(person);
+        System.out.println("Json:"+ json);
+
+        Person person2 = gson.fromJson(json, Person.class);
+        System.out.println("Parsed person:"+person2.name+", "+person2.age );
     }
 }
